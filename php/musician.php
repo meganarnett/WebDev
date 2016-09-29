e0d93574744a2eff82c923410ce351207b201196
Megan Michaela Arnett <marnett@onyx.boisestate.edu>
2016-09-28 21:37:23 -0600

    webpage

--- php/musician.php
+++ php/musician.php
@@ -1,2 +1,24 @@
-<?php
-	hello
+<html>
+	<head>
+        <link rel="stylesheet" type="text/css" href="homepage.css">
+    </head>
+    <body>
+        <div id="header">
+            <h1 class="name">Music Connect</h1>
+            <div class= "login">
+                <li> Log In</li>
+            </div>
+            <ul class="navigation">
+                <li class="navLink">
+                    <a href="/~marnett/cs401/php/musician.php">Musicians</a>
+                </li>
+                <li class="navLink">
+                    <a href="/~marnett/cs401/php/teacher.php">Teachers</a>
+                </li>
+                <li class="navLink">
+                    <a href="/~marnett/cs401/php/school.php">Schools</a>
+                </li>
+            </ul>
+        </div>
+
+</html>
--- php/teacher.php
+++ php/teacher.php
@@ -1,1 +1,24 @@
-teacher
+<html>
+	<head>
+        <link rel="stylesheet" type="text/css" href="homepage.css">
+    </head>
+    <body>
+        <div id="header">
+            <h1 class="name">Music Connect</h1>
+            <div class= "login">
+                <li> Log In</li>
+            </div>
+            <ul class="navigation">
+                <li class="navLink">
+                    <a href="/~marnett/cs401/php/musician.php">Musicians</a>
+                </li>
+                <li class="navLink">
+                    <a href="/~marnett/cs401/php/teacher.php">Teachers</a>
+                </li>
+                <li class="navLink">
+                    <a href="/~marnett/cs401/php/school.php">Schools</a>
+                </li>
+            </ul>
+        </div>
+
+</html>
