b45857e6d590bf9ca27e96d40694757b487fd7b6
meganarnett <meganarnett@u.boisestate.edu>
2016-11-14 17:02:29 -0700

    Update Dao.php

--- Dao.php
+++ Dao.php
@@ -1,21 +1,21 @@
 <?php
 class Dao {
-/*	private $host = "us-cdbr-iron-east-04.cleardb.net";
+	private $host = "us-cdbr-iron-east-04.cleardb.net";
 	private $db = "heroku_d49a7d31efccd7";
 	private $user = "bab4dd1cb60954";
 	private $pass = "f5dffe97";
-
+/*
 	pubic function getConnection() {
 		return
 		new PDO("mysql:host={$this->$host};dbname={$this->$dbname};
 	}
-*/
+
 	private $host = "localhost";
 	private $db = "marnett";
 	private $user = "root";
 	private $pass = "";
 	private $log;
-
+*/
 
 	public function getConnection() {
 		$conn = new PDO("mysql:host={$this->host};dbname={$this->db}", "$this->user", "$this->pass");
