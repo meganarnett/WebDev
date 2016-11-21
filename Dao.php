b0e5c625646980a8c92f179a533af031a3cb9929
meganarnett <meganarnett@u.boisestate.edu>
2016-11-21 11:14:26 -0800

    Update Dao.php

--- Dao.php
+++ Dao.php
@@ -1,10 +1,10 @@
 <?php
 class Dao {
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$server = $url["us-cdbr-iron-east-04.cleardb.net"];
-	$username = $url["bab4dd1cb60954"];
-	$password = $url["f5dffe97"];
-	$db = substr($url["heroku_d49a7d31efccd7"], 1);
+	private $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	private $server = $url["us-cdbr-iron-east-04.cleardb.net"];
+	private $username = $url["bab4dd1cb60954"];
+	private $password = $url["f5dffe97"];
+	private $db = substr($url["heroku_d49a7d31efccd7"], 1);
 //	$conn = new mysqli($server, $username, $password, $db);
 
 /*	private $host = "us-cdbr-iron-east-04.cleardb.net";
